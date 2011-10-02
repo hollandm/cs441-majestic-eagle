7abd92f5f0a4c1556eabba9f5723db95e64910bf
Tanya L. Crenshaw <tanya.crenshaw@gmail.com>
2011-10-02 11:19:39 -0700

    The R file evalGPA.r is now parameterized on target zip code.
    For any given zip code, it will calculate the average high school GPA
    among applicants for all .csv files in the current directory.

--- adminData/evalGPA.r
+++ adminData/evalGPA.r
@@ -1,23 +1,31 @@
 # file: evalGPA 
-#
+
 # Written by: Tanya L. Crenshaw 
-#
+
 # Description: This R function processes .csv files in the current
 # directory.  These .csv files are assumed to be UP admissions data,
 # one .csv file for each year.  To "process" a .csv file, the average
-# high school GPA for students applying to UP from zip code 97303
-# is calculated.  A data frame of mean GPAs per year for 97303
-# is returned as well as saved in an external file.
-#
+# high school GPA for students applying to UP from the target zipcode
+# is calculated.  A data frame of mean GPAs per year for the target
+# zipcode is returned as well as saved in an external file in the
+# "auto" subdirectory.
+
+# Assumption: The .csv files for the UP admissions data have been
+# preprocessed such that the zipcode is a 5-digit numeric value and
+# the special 4-digit code has been placed in another column.  What
+# once was 97217-1524 is now 97217 in one column and 1524 in another.
+# This is easily done in Excel using the "Text to Columns" option.
+
 # To run the script use:
 # 
 #  source("evalGPA.r");
-#  result = evalGPA();
+#  result = evalGPA(<target zipcode>);
 #
-# a file called gpa.csv will be created in the 97303 sub-directory
-# in the current directory.
+# a file called gpa.csv will be created in the target zipcode
+# sub-directory in the current directory.
 
-evalGPA <- function() {
+
+evalGPA <- function(zipcode) {
 
   # initialize results vectors
   means = NULL;			# the mean GPA for a particular year
@@ -36,7 +44,7 @@ evalGPA <- function() {
 
     # obtain the gpa scores for people in zip code 97303
     # who *applied* to the university of portland
-    gpaScores <- admissionsData$HS_GPA[admissionsData$Zip == 97303 & admissionsData$Applied=="Y", drop=TRUE];
+    gpaScores <- admissionsData$HS_GPA[admissionsData$Zip == zipcode & admissionsData$Applied=="Y", drop=TRUE];
 
     # convert the result from R-factors to numeric values
     gpaScores = as.numeric(as.character(gpaScores));
@@ -62,11 +70,17 @@ evalGPA <- function() {
   # create a data frame of results
   result = (data.frame(meanGpa = means, total=totalApplicants, numReported=reportingApplicants,year=years));
 
+  # construct the pathname in which to store the results
+  path <- paste("auto/", zipcode, sep="");
+
   # create a sub-directory in "auto" to store the results
-  dir.create("auto/97303");
+  dir.create(path);
+
+  # construct the filename in which to store the results
+  filename <- paste(path, "/gpa.csv", sep="");
 
   # write the data frame to an external .csv file 
-  write.csv(result,"auto/97303/gpa.csv"); 
+  write.csv(result,filename); 
 
   # return the data frame of results
   return (result);
