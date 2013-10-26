6b054137b39da88c9abec849b88adb2042e531c6
Matt Holland <mbholland333@gmail.com>
2013-10-26 12:15:20 -0700

    Began having studnt info populate high schools. Ran into issue that most high schools don't had addresses assosiated with them.

--- highSchool.js
+++ highSchool.js
@@ -9,14 +9,11 @@ cs441GoogleMapsViz.highShool = {};
  *
  * The High School object 
  */
-
 cs441GoogleMapsViz.highShool.highSchool = function(ceeb, name, location) {
 	
-	//alert("Creating HS Object");
 	this.ceeb = ceeb;
 	this.name = name;
 	this.location = location;
-	this.display = true;
 	
 	this.gpa = 0;
 	this.sat = 0;
@@ -24,60 +21,88 @@ cs441GoogleMapsViz.highShool.highSchool = function(ceeb, name, location) {
 	
 }
 
-function filterLayers() {
-	console.log(cs441GoogleMapsViz.httpRequest2);
-	if (cs441GoogleMapsViz.httpRequest2.readyState === 4) {
-		console.log(cs441GoogleMapsViz.httpRequest2);
-		if(cs441GoogleMapsViz.httpRequest2.status === 200) {
-			console.log(cs441GoogleMapsViz.httpRequest2);
-			console.log(cs441GoogleMapsViz.httpRequest2.responseText);
-			console.log(cs441GoogleMapsViz.httpRequest2);
-			//alert("Filter");
+/**
+ * loadHighschools
+ * 
+ * 
+ */
+function loadHighschools() {
+	if (cs441GoogleMapsViz.HShttpRequest.readyState === 4) {
+		if(cs441GoogleMapsViz.HShttpRequest.status === 200) {
+	
 			
 			// The code reaches this point because the Google server
 			// responded with some useful data.
-			console.log(cs441GoogleMapsViz.httpRequest2.responseText);
+			//console.log(cs441GoogleMapsViz.HShttpRequest.responseText);
 		
 			// The response is just a string.  I need
 			// to parse it so that I can extract the zip code from it.
-			var response = JSON.parse(cs441GoogleMapsViz.httpRequest2.responseText);
+			var response = JSON.parse(cs441GoogleMapsViz.HShttpRequest.responseText);
 
-			console.log(response);
+			//console.log(response);
 			
-			/*
+			cs441GoogleMapsViz.schools = {}
 			
-			if(response["rows"] != undefined) {
-				// Set 	the zoom.
-				layerArray[0].map.setZoom(11);
+			for(var i = 0; i < response.rows.length; i++) {
+				var hs = response.rows[i];
 
-				var zipcode = response["rows"][0].toString();
-
-				// Center the map.
-				centerAt(layerArray[0].map, zipcode, geocoder);
-
-				// Filter the zip layer by zip
-				cs441GoogleMapsViz.filterByZip.call(layerArray[0], zipcode);
+				cs441GoogleMapsViz.schools[hs[0]] = new cs441GoogleMapsViz.highShool.highSchool(hs[0], hs[1], hs[2]);
 				
-				// Filter the school layer by CEEB.
-				cs441GoogleMapsViz.filterByCEEB.call(layerArray[1], ceeb);
-				
-				
-			} else {
-				// Indicate to the user that I could not find that
-				// CEEB.
-				cs441GoogleMapsViz.getErrorMsgElement().innerHTML = "Cannot locate CEEB: " + ceeb + ".";
 			}
-			*/
+			
+			console.log(cs441GoogleMapsViz.schools)
+			
+			cs441GoogleMapsViz.highShool.refreshStats()
+			
 		} 
 	}
-	console.log(cs441GoogleMapsViz.httpRequest2);
 }
 
-//console.log(cs441GoogleMapsViz);
+/**
+ * refreshStats
+ * 
+ */
+cs441GoogleMapsViz.highShool.refreshStats = function() {
+	
+	cs441GoogleMapsViz.statsHttpRequest = new XMLHttpRequest();
+
+	function respondStats() {
+		if (cs441GoogleMapsViz.statsHttpRequest.readyState === 4) {
+			if(cs441GoogleMapsViz.statsHttpRequest.status === 200) {
+		
+				var response = JSON.parse(cs441GoogleMapsViz.statsHttpRequest.responseText);
+				
+				
+				
+				cs441GoogleMapsViz.schools[response[0]].students += 1;
+				cs441GoogleMapsViz.schools[response[0]].gpa += response[1];
+				//cs441GoogleMapsViz.schools[response[0]].sat += (response[2] + response[3])/2; //TODO: Have average in the database to avoid extra math
+				
+				console.log(response)
+				
+			}
+		}
+	}
+	
+	//var query = "SELECT 'Code', 'HighSchool', 'Address' FROM " + schoolEID;
+	var query = "SELECT 'HighSchoolCode','HS_GPA', 'SAT_MAth', 'SAT_Verbal', 'Application_Status', 'App_Decision_Code', 'Confirmed', 'Enrolled' FROM 13na5H4_enS7_zftNnAhsd1JWpgDBVv6tg5P_624";
+	//
+		
+	var url = "https://www.googleapis.com/fusiontables/v1/query";
+	url = url + "?sql=";
+	url = url + query;
+	url = url + " &key=" + cs441GoogleMapsViz.apikey;
+	
+	cs441GoogleMapsViz.statsHttpRequest.onreadystatechange = respondStats;
+	cs441GoogleMapsViz.statsHttpRequest.open('GET', url);
+	cs441GoogleMapsViz.statsHttpRequest.send();
+
+}
+
 cs441GoogleMapsViz.apikey = 'AIzaSyCGPkL4Q0Ki278FcPmJAjlMIzwQPtyiLdk'
 
 var schoolEID = '1TysRKf1siV396AMbUKmi8w2-XB3Zeye2ObXjl8Y';	
-var query = "SELECT 'HighSchool' FROM " + schoolEID;
+var query = "SELECT 'Code', 'HighSchool', 'Address' FROM " + schoolEID;
 
 
 var url = "https://www.googleapis.com/fusiontables/v1/query";
@@ -85,25 +110,14 @@ url = url + "?sql=";
 url = url + query;
 url = url + " &key=" + cs441GoogleMapsViz.apikey;
 
-//url = "http://www.google.com"
 
-cs441GoogleMapsViz.httpRequest2 = new XMLHttpRequest();
+cs441GoogleMapsViz.HShttpRequest = new XMLHttpRequest();
 
 // Send the GET Request to the Google Server
-//cs441GoogleMapsViz.sendRequest(url, filterLayers);
+//cs441GoogleMapsViz.sendRequest(url, loadHighschools);
 
-cs441GoogleMapsViz.httpRequest2.onreadystatechange = filterLayers;
-cs441GoogleMapsViz.httpRequest2.open('GET', url);
-cs441GoogleMapsViz.httpRequest2.send();
-
-
- 
-var hss = {};
-var index = 0;
-
-for (index = 0; index < 5000; ++index) {
-	hss[index] = cs441GoogleMapsViz.highShool.highSchool(123, "ABC", "ABC");
-}
-
+cs441GoogleMapsViz.HShttpRequest.onreadystatechange = loadHighschools;
+cs441GoogleMapsViz.HShttpRequest.open('GET', url);
+cs441GoogleMapsViz.HShttpRequest.send();
 
 //alert("HS Database Loaded");
