a750f9cd4296bbed6baf95a9266e524e73a15e8e
Matt Holland <mbholland333@gmail.com>
2013-10-27 17:39:07 -0700

    Scaped address data all high schools.

--- highSchoolData/highSchoolDataScraper.py
+++ highSchoolData/highSchoolDataScraper.py
@@ -34,25 +34,40 @@ import urllib2, re
 from bs4 import BeautifulSoup
 
 # Open a file to write .csv data
-f = open("OregonHighSchools_raw.csv", 'w')
+f = open("HighSchools_raw.csv", 'w')
 
 # Write the headers for the .csv data
 f.write("High School, StreetAddress, City, State, Zip\n")
 
+
 # Create a list of links to scrape for the state of Oregon.
 urls = []
-urls.append("http://www.directoryofschools.com/high-schools/oregon.htm")
-urls.append("http://www.directoryofschools.com/high-schools/Oregon-High-Schools-2.html")
-urls.append("http://www.directoryofschools.com/high-schools/Oregon-High-Schools-3.html")
-urls.append("http://www.directoryofschools.com/high-schools/Oregon-High-Schools-4.html")
+states = [
+"Alabama","Alaska","Arizona","Arkansas","California","Colorado","Connecticut","Delaware","Florida","Georgia","Hawaii","Idaho","Illinois","Indiana","Iowa","Kansas","Kentucky","Louisiana","Maine","Maryland","Massachusetts","Michigan","Minnesota","Mississippi","Missouri","Montana","Nebraska","Nevada","New Hampshire","New Jersey","New Mexico","New York","North Carolina","North Dakota","Ohio","Oklahoma","Oregon","Pennsylvania","Rhode Island","South Carolina","South Dakota","Tennessee","Texas","Utah","Vermont","Virginia","Washington","West Virginia","Wisconsin","Wyoming"
+]
 
+
+for state in states:    
+    urls.append("http://www.directoryofschools.com/high-schools/"+state+".html")
+    urls.append("http://www.directoryofschools.com/high-schools/"+state+"-High-Schools-2.html")
+    urls.append("http://www.directoryofschools.com/high-schools/"+state+"-High-Schools-3.html")
+    urls.append("http://www.directoryofschools.com/high-schools/"+state+"-High-Schools-4.html")
+    urls.append("http://www.directoryofschools.com/high-schools/"+state+"-High-Schools-5.html")
+    urls.append("http://www.directoryofschools.com/high-schools/"+state+"-High-Schools-6.html")
+    urls.append("http://www.directoryofschools.com/high-schools/"+state+"-High-Schools-7.html")
+    urls.append("http://www.directoryofschools.com/high-schools/"+state+"-High-Schools-8.html")
+    urls.append("http://www.directoryofschools.com/high-schools/"+state+"-High-Schools-9.html")
 
 # Parse each url and extract a list of high schools.
 for page in urls:
     # Load the page; This loads all of the HTML that the URL points
     # to.
-    page = urllib2.urlopen(page)
-    
+
+    try:
+        page = urllib2.urlopen(page)
+    except:
+        continue
+
     # Use BeautifulSoup to parse the page.  The raw HTML is now stored as
     # a collection of elements that are much easier to work with.
     soup = BeautifulSoup(page)
@@ -67,58 +82,26 @@ for page in urls:
     
     # Assemble each entry
     for i in range(0, len(schoolNames)-2):
-        f.write(schoolNames[i+2].string + ",")  # The name of the high school
- 
-        f.write(schoolAddrs[i].contents[0] + "," + schoolAddrs[i].contents[2]) # The address information
-     
+        try:
+            f.write(schoolNames[i+2].string + ",")  # The name of the high school
+        except:
+            print "Cant Write " + schoolNames[i+2].string + "\n"
+
+        try:
+            f.write(schoolAddrs[i].contents[0] + "," + schoolAddrs[i].contents[2]) # The address information
+        except:
+            try:
+                f.write(schoolAddrs[i].contents[0] + "," + schoolAddrs[i].contents[1]) # The address information
+            except:
+                print str(i)
+
         # This might be an extra-long entry.  
-        if len(schoolAddrs[i].contents) > 7:
-               f.write("," + schoolAddrs[i].contents[4])
+        #if len(schoolAddrs[i].contents) > 7:
+        #       f.write("," + schoolAddrs[i].contents[4])
             
         f.write("\n")        
         
 # Close the file
 f.close()
 
-# Reopen the file and do some corrections.  The data scraped from
-# these websites do not have consistent formatting.
-input = open("OregonHighSchools_raw.csv", 'r')
-output = open("OregonHighSchools_clean.csv","w")
 
-# The search pattern for one of the file corrections below is "P O
-# Box" followed by any set of decimal values.  Note that the website
-# isn't consistent with P.O. or PO or whatever so all possibilities
-# are represented.  There's probably a better way to represent this
-# with a regex in Python, but I think this is more readable. 
-pattern= re.compile("P. O. Box \d*|PO Box \d*|P O Box \d*|P.O. Box \d*|PO Box BB|P.O. Box W|PO, Box \d*") 
-
-# Plow through the file line by line, making corrections.
-for line in input:
-
-    # Correction.  Replace any instance of ",," with ","
-    line = line.replace(",,",",")
-
-    # Correction.  Replace any instance of Oregon with OR
-    line = line.replace("Oregon","OR")
-
-    # Correction.  Replace any instance of OR with OR, so that the zipcode is
-    # in its own column in .csv.
-    line = line.replace("OR","OR,")
-
-    # Correction.  Some schools have both a street address and
-    # P.O. Box listed while other schools have only a P.O. Box.
-    # Unconditionally remove all P.O. Boxes.  This means that some
-    # high schools will have no street address, but the essential data
-    # for my purpose is zipcode.
-    line = re.sub(pattern, "", line)
-
-    # Correction. If a PO Box was removed, there might be an extra
-    # comma, so again replace any instance of ",," with ","
-    line = line.replace(",,",",")
-    line = line.replace(", ,",",")
-
-    output.write(line)
-
-
-# Corrections complete.  Close the file.
-f.close()
