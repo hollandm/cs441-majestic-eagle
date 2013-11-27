c4c08efd773b6b6a36c954d27a32ae5af6ed725c
Allie Seibert <Owner@.(none)>
2013-11-26 17:01:51 -0800

    fix zoom for single datapoint

--- usmap.js
+++ usmap.js
@@ -183,17 +183,21 @@ cs441GoogleMapsViz.displayMapMarkers = function() {
 	cs441GoogleMapsViz.markers = [];
 	
 	var bounds = new google.maps.LatLngBounds();
-
 	// For every school with students, add a marker to the map
 	for (ceeb in cs441GoogleMapsViz.highSchools) {
 		var school = cs441GoogleMapsViz.highSchools[ceeb];
 		if (school.isActive) {
 			var marker = new cs441GoogleMapsViz.hsMarker(school);
-			bounds.extend(marker.marker.position);	cs441GoogleMapsViz.markers.push(marker);
+			bounds.extend(marker.marker.position);
+			cs441GoogleMapsViz.markers.push(marker);
 		}
 	}
-
+	
 	cs441GoogleMapsViz.map.fitBounds(bounds);
+	
+	if (cs441GoogleMapsViz.markers.length == 1){
+		cs441GoogleMapsViz.map.setZoom(7);
+	}
 	
 };
 	
